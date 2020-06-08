7edf9d6d88e01798b50cea5dbcddf5f5393450b2
Benjamin Moss <drteeth@gmail.com>
2020-06-08 17:51:19 -0400

    disable migrations temporarily.

--- rel/hooks/pre_start/migrate.sh
+++ rel/hooks/pre_start/migrate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Running migrations"
-bin/slack_noodling migrate
-echo "Migrations run successfully"
+# echo "Running migrations"
+# bin/slack_noodling migrate
+# echo "Migrations run successfully"
